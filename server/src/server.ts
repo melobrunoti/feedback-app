f157ca491516266eca8e5376e694384e82a2ce71
melobrunoti <melobrunoti@gmail.com>
2022-06-13 13:21:39 -0300

    add cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,22 +1,14 @@
 /* eslint-disable import/no-unresolved */
 import express from 'express';
-import nodemailer from 'nodemailer';
-import { prisma } from './prisma';
+import cors from 'cors';
+
 import routes from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
-
-const transport = nodemailer.createTransport({
-  host: 'smtp.mailtrap.io',
-  port: 2525,
-  auth: {
-    user: '5809e5348f8142',
-    pass: 'b20effe25200ef',
-  },
-});
 
 app.listen(3333, () => {
   console.log('Http server running!');
